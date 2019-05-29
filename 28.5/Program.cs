38076c30653128590b7dfbab427c31c24418265a
batfiowoof <51147711+batfiowoof@users.noreply.github.com>
2019-05-29 07:52:51 +0300

    Initial commit

---
+++ 28.5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._5
+{
+    class Program
+    {
+        static void PrintString(char[] arr1,int l)
+        {
+
+            char ch; //promenliva za bukvite
+            for (int x = 0; x < l; x++)
+            {
+                ch = arr1[x];
+                if (Char.IsLower(ch)) Console.Write(Char.ToUpper(ch)); //proverka dali bukvata e malka, ako e, preobrazuva se v golqma(1)
+                else Console.Write(Char.ToLower(ch)); //obratnoto na (1)
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            //zadacha - preobrazuvane na malki bukvi v golemi i obratnoto v string
+            Console.WriteLine("Enter string");
+            char[] arr1; //masiv za bukvite
+            int duljina; //int za duljinata na stringa
+            string str = Console.ReadLine(); //vuvejdane na stringa
+            duljina = str.Length; //izvejdane na duljinata na stringa kum promenlivata
+            arr1 = str.ToCharArray(0, duljina); //preobrazuvane na stringa v bukvi 
+            PrintString(arr1,duljina); //vikane na metoda ^
+            
+            Console.ReadKey();
+        }
+    }
+}
